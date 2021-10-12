c73651685a44f59e3b9827378e564df8a2a1d5f8
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-12 22:58:29 +0700

    Create pages_controller.php

---
+++ controllers/pages_controller.php
@@ -0,0 +1,9 @@
+<?php
+class PageController
+{
+    public function home()
+    { require_once('views/page/home.php');}
+    public function error()
+    { require_once("views/page/error.php");}
+}
+?>
