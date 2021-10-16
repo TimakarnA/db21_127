588ec0772e0bc2e59e485a7d7ea2eba6234c3fee
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-16 22:32:30 +0700

    Update fieldhospital_controller.php

--- controllers/fieldhospital_controller.php
+++ controllers/fieldhospital_controller.php
@@ -2,7 +2,8 @@
 {
     public function index()
     {
-        $fieldhospital_list = price_detail::getAll();
-        require_once("./views/price_detail/index_pricedetail.php");
+        //echo "00000";
+        $fieldhospital_list = field_hospital::getAll();
+        require_once("./views/field_hospital/index_fieldhospital.php");
     }
-}
+}?>
