3be60d197943c7cfed0d8fc7199726d7bdefda53
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-17 21:14:51 +0700

    Update fieldhospital.php

--- model/fieldhospital.php
+++ model/fieldhospital.php
@@ -2,16 +2,18 @@
 class field_hospital{
     public $FHID;
     public $FHName;
+    public $FHaddress;
     public $FHdate;
     public $greenbed;
     public $yellowbed;
     public $redbed;
     public $AID;
 
-    public function __construct($FHID,$FHName,$FHdate,$greenbed,$yellowbed,$redbed,$AID)
+    public function __construct($FHID,$FHName,$FHaddress,$FHdate,$greenbed,$yellowbed,$redbed,$AID)
     {
         $this->FHID = $FHID;
         $this->FHName = $FHName;
+        $this->FHaddress = $FHaddress;
         $this->FHdate = $FHdate;
         $this->greenbed = $greenbed;
         $this->yellowbed = $yellowbed;
@@ -23,7 +25,7 @@ class field_hospital{
         //echo "55555";
         $fieldhospital_list=[];
         require("connection_connect.php");
-        $sql ="SELECT field_hospital.FHID,field_hospital.FHName,field_hospital.FHdate,field_hospital.greenbed,field_hospital.yellowbed,field_hospital.redbed,agency.name as AID
+        $sql ="SELECT field_hospital.FHID,field_hospital.FHName,field_hospital.FHaddress,field_hospital.FHdate,field_hospital.greenbed,field_hospital.yellowbed,field_hospital.redbed,agency.name as AID
         FROM field_hospital INNER JOIN agency ON agency.id=field_hospital.AID" ;
         //$sql="SELECT * from field_hospital";
         $result=$conn->query($sql);
@@ -32,12 +34,13 @@ class field_hospital{
         {
             $FHID = $my_row[FHID];
             $FHName = $my_row[FHName];
+            $FHaddress = $my_row[FHaddress];
             $FHdate = $my_row[FHdate];
             $greenbed =$my_row[greenbed];
             $yellowbed = $my_row[yellowbed];
             $redbed =$my_row[redbed];
             $AID = $my_row[AID];
-            $fieldhospital_list[]= new field_hospital($FHID,$FHName,$FHdate,$greenbed,$yellowbed,$redbed,$AID);
+            $fieldhospital_list[]= new field_hospital($FHID,$FHName,$FHaddress,$FHdate,$greenbed,$yellowbed,$redbed,$AID);
         }
 
         require("connection_close.php");
@@ -47,48 +50,50 @@ class field_hospital{
     {
         //echo"iiiiii";
         require("connection_connect.php");
-        $sql ="SELECT field_hospital.FHID,field_hospital.FHName,field_hospital.FHdate,field_hospital.greenbed,field_hospital.yellowbed,field_hospital.redbed,agency.name as AID
+        $sql ="SELECT field_hospital.FHID,field_hospital.FHName,field_hospital.FHaddress,field_hospital.FHdate,field_hospital.greenbed,field_hospital.yellowbed,field_hospital.redbed,agency.name as AID
         FROM field_hospital INNER JOIN agency ON agency.id=field_hospital.AID" ;
         $result=$conn->query($sql);
         $my_row=$result->fetch_assoc();
         $FHID = $my_row[FHID];
         $FHName = $my_row[FHName];
+        $FHaddress = $my_row[FHaddress];
         $FHdate = $my_row[FHdate];
         $greenbed =$my_row[greenbed];
         $yellowbed = $my_row[yellowbed];
         $redbed =$my_row[redbed];
         $AID = $my_row[AID];
         require("connection_close.php");
-        return new field_hospital($FHID,$FHName,$FHdate,$greenbed,$yellowbed,$redbed,$AID);
+        return new field_hospital($FHID,$FHName,$FHaddress,$FHdate,$greenbed,$yellowbed,$redbed,$AID);
     }
     public static function search($key)
     {
         require("connection_connect.php");
-        $sql ="SELECT field_hospital.FHID,field_hospital.FHName,field_hospital.FHdate,field_hospital.greenbed,field_hospital.yellowbed,field_hospital.redbed,agency.name as AID
+        $sql ="SELECT field_hospital.FHID,field_hospital.FHName,field_hospital.FHaddress,field_hospital.FHdate,field_hospital.greenbed,field_hospital.yellowbed,field_hospital.redbed,agency.name as AID
         FROM field_hospital INNER JOIN agency ON agency.id=field_hospital.AID
-        Where (field_hospital.FHID LIKE '%$key%' OR field_hospital.FHName LIKE '%$key%' OR field_hospital.FHdate LIKE '%$key%' OR field_hospital.greenbed LIKE '%$key%' OR 
+        Where (field_hospital.FHID LIKE '%$key%' OR field_hospital.FHName LIKE '%$key%' OR field_hospital.FHaddress LIKE '%$key%' OR field_hospital.FHdate LIKE '%$key%' OR field_hospital.greenbed LIKE '%$key%' OR 
         field_hospital.yellowbed LIKE '%$key%' OR field_hospital.redbed LIKE '%$key%' OR agency.name LIKE '%$key%')" ;
         $result=$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
             $FHID = $my_row[FHID];
             $FHName = $my_row[FHName];
+            $FHaddress = $my_row[FHaddress];
             $FHdate = $my_row[FHdate];
             $greenbed =$my_row[greenbed];
             $yellowbed = $my_row[yellowbed];
             $redbed =$my_row[redbed];
             $AID = $my_row[AID];
-            $fieldhospital_list[]= new field_hospital($FHID,$FHName,$FHdate,$greenbed,$yellowbed,$redbed,$AID);
+            $fieldhospital_list[]= new field_hospital($FHID,$FHName,$FHaddress,$FHdate,$greenbed,$yellowbed,$redbed,$AID);
         }
 
         require("connection_close.php");
         return $fieldhospital_list ;
     }
-    public static function Add($FHID,$FHName,$FHdate,$greenbed,$yellowbed,$redbed,$AID)
+    public static function Add($FHID,$FHName,$FHaddress,$FHdate,$greenbed,$yellowbed,$redbed,$AID)
     {
         //echo "00000";
         require("connection_connect.php");
-        $sql ="INSERT INTO field_hospital (FHID,FHName,FHdate,greenbed,yellowbed,redbed,AID) VALUES ('$FHID','$FHName','$FHdate','$greenbed','$yellowbed','$redbed','$AID')";
+        $sql ="INSERT INTO field_hospital (FHID,FHName,FHaddress,FHdate,greenbed,yellowbed,redbed,AID) VALUES ('$FHID','$FHName','$FHaddress','$FHdate','$greenbed','$yellowbed','$redbed','$AID')";
         $result=$conn->query($sql);
         require("connection_close.php");
         return "Add success $result rows";
