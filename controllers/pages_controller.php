191f7476505d8c377d9320c9c7d8060c2afa2149
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-17 15:17:43 +0700

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -1,9 +1,9 @@
 <?php
-class PageController
+class PagesController
 {
     public function home()
-    { require_once('views/page/home.php');}
+    { require_once("views/pages/home.php");}
     public function error()
-    { require_once("views/page/error.php");}
+    { require_once("views/pages/error.php");}
 }
 ?>
