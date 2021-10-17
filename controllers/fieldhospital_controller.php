2074480530eb1c90a789492c9b43981fca6b0c3e
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-17 15:17:52 +0700

    Update fieldhospital_controller.php

--- controllers/fieldhospital_controller.php
+++ controllers/fieldhospital_controller.php
@@ -6,4 +6,29 @@
         $fieldhospital_list = field_hospital::getAll();
         require_once("./views/field_hospital/index_fieldhospital.php");
     }
+    public function newFieldhospital()
+    {
+        $agency_list = Agency::getAll();
+        require_once("./views/field_hospital/newFieldhospital.php");
+    }
+    public function addFieldhospital()
+    {
+       //echo "000000";
+       $FHID = $_GET['FHID'];
+       $FHName = $_GET['FHName'];
+       $FHdate = $_GET['FHdate'];
+       $greenbed = $_GET['greenbed'];
+       $yellowbed = $_GET['yellowbed'];
+       $redbed = $_GET['redbed'];
+       $AID = $_GET['AID'];
+       field_hospital::Add($FHID,$FHName,$FHdate,$greenbed,$yellowbed,$redbed,$AID);
+       
+       FieldhospitalController::index();
+    }
+    public function search()
+    {
+        $key = $_GET['key'];
+        $fieldhospital_list = field_hospital::search($key);
+        require_once("./views/field_hospital/index_fieldhospital.php");
+    }
 }?>
