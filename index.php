c7370b9e1eff874fdb23cc3fe1970eb3249192fd
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-10-28 16:12:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
     <br>
     [<a href="?controller=pages&action=home">Home</a>]
     [<a href="?controller=fieldhospital&action=index">field_hospital</a>]
-    [<a href="?controller=patient&action=index">patient</a>]
+    [<a href="?controller=patientinfh&action=index">patientinfh</a>]
     [<a href="?controller=telemedicine&action=index">telemedicine</a>]
     <br>
     <?php require_once("./routes.php") ; ?>
