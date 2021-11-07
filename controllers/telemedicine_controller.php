c50115e8cac1d346fb301ed217ec024f632df0d6
TimakarnA <91516456+TimakarnA@users.noreply.github.com>
2021-11-07 17:50:23 +0700

    Update telemedicine_controller.php

--- controllers/telemedicine_controller.php
+++ controllers/telemedicine_controller.php
@@ -23,7 +23,8 @@
        $symptom = $_GET['symptom'];
        $temperature = $_GET['temperature'];
        $teledate = $_GET['teledate'];
-       telemedicine::Add($teleID,$patient_id,$symptom,$temperature,$teledate);
+       $staffinfh = $_GET['staffinfh'];
+       telemedicine::Add($teleID,$patient_id,$symptom,$temperature,$teledate,$staffinfh);
        TelemedicineController::index();
     }
     public function search()
@@ -31,6 +32,28 @@
         $key = $_GET['key'];
         $telemedicine_list = telemedicine::search($key);
         require_once("./views/telemedicine/index_telemedicine.php");
+    }
+    public function updateForm()
+    {
+        //echo $teleID;
+        $teleID = $_GET['teleID'];
+        $telemedicine = telemedicine::get($teleID);
+        $patientinfh_list = patientinfh::getAll();
+        //$patient_list = patient::getAll();
+        $people_list = people::getAll();
+        require_once("./views/telemedicine/updateForm.php");
+    }
+    public function update()
+    {
+        $teleID = $_GET['teleID'];
+        $patient_id = $_GET['patient_id'];
+        $NEWID = $_GET['ID'];
+        $symptom = $_GET['symptom'];
+        $temperature = $_GET['temperature'];
+        $teledate = $_GET['teledate'];
+        $staffinfh = $_GET['staffinfh'];
+        telemedicine::update($teleID,$patient_id,$symptom,$temperature,$teledate,$staffinfh,$NEWID);
+        TelemedicineController::index();
     }
     
     /*public function deleteConfirm()
